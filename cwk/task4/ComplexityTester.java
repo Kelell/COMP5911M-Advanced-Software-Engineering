b057e959381f063d18e3bd4815b9896c22a3e777
ll16kdt <ll16kdt@leeds.ac.uk>
2020-12-06 20:53:25 +0000

    extended CalculateComplexity() to calculate the average and maximum complexity of the directory

--- cwk/task4/ComplexityTester.java
+++ cwk/task4/ComplexityTester.java
@@ -23,7 +23,6 @@ public class ComplexityTester {
 
    //Follow the directory path and get all the file locations
    static File[] GetDirectoryPath(String path) throws FileNotFoundException{
-    //Creating a File object for directory
     File directoryPath = new File(path);
     //List of all files and directories
     FilenameFilter textFilefilter = new FilenameFilter(){
@@ -49,6 +48,7 @@ public class ComplexityTester {
    try {
      File f = new File(path);
      byte[] bf = new byte[(int)f.length()];
+     //in order to remove all the comments, we cannot read the file line by line
      new FileInputStream(f).read(bf);
      fileContent = new String(bf, "UTF-8");
      //remove all comments from the string
@@ -58,12 +58,12 @@ public class ComplexityTester {
      for(int i = 0; i < tokens.length; i++){
        if(tokens[i].trim().length() > 0){
          temp=tokens[i].replaceAll("\\s","");
+         //check for keywords
          if(temp.matches(".*if.*|.*while.*|.*for.*|.*case.*|.*switch.*|.*try.*|.*catch.*|.*\\|\\|.*|.*&&.*|.*\\|\\|.*|.*&&.*")){
            keyWords++;
          }
        }
      }
-     //System.out.println("Number of keywords: "+keyWords);
  } catch (FileNotFoundException e) {
      e.printStackTrace();
  } catch (IOException e) {
@@ -77,20 +77,16 @@ static int InspectClass(String directoryPath, String noExtension) throws ClassNo
     @SuppressWarnings("unchecked")
         URL classUrl;
         int numberOfMethods=0;
-        //this line needs to be changed to take a classpath
         String directoryURL= ("file://"+directoryPath+"/");
         classUrl = new URL(directoryURL);
         URL[] classUrls = { classUrl };
         URLClassLoader ucl = new URLClassLoader(classUrls);
-        //This would be a for() loop to get every .class file in the directory
         Class c = ucl.loadClass(noExtension);
         try {
            Method m[] = c.getDeclaredMethods();
            String className = c.getName();
            numberOfMethods=0;
-           //System.out.println(className+ " Methods are: "); //testing line
            for (int i = 0; i < m.length; i++){
-             //System.out.println(m[i].toString()); //testing line
              numberOfMethods++;
            }
         }
@@ -101,6 +97,11 @@ static int InspectClass(String directoryPath, String noExtension) throws ClassNo
     }
 
     static void CalculateComplexity(String path, File filesList[]) {
+      int numberOfFiles=0;
+      int totalComplexity=0;
+      int averageComplexity;
+      int highestCompexity=-1;
+      String highestFile="";
       try {
         for(File file : filesList) {
           //Use regex to remove the class extension, required for Relefection API
@@ -108,11 +109,24 @@ static int InspectClass(String directoryPath, String noExtension) throws ClassNo
            System.out.println("File name: "+ noExtension);
            int numberOfKeyWords=ReadFile(file.getAbsolutePath().toString());
            int numberOfMethods=InspectClass(path, noExtension);
+           //testing lines
            //System.out.println("Has "+ numberOfMethods+ " methods");
            //System.out.println("Has "+ numberOfKeyWords+ " keywords");
            int cyclomaticComplexity= numberOfKeyWords+numberOfMethods;
            System.out.println("Has a complexity of: "+cyclomaticComplexity);
+           //calculate the average complexity
+           numberOfFiles++;
+           totalComplexity=totalComplexity + cyclomaticComplexity;
+           //calculate the maximum complexity
+           if (cyclomaticComplexity > highestCompexity){
+             highestCompexity=cyclomaticComplexity;
+             highestFile= noExtension;
+           }
       }
+      averageComplexity=(totalComplexity / numberOfFiles);
+      System.out.println("\n");
+      System.out.println("The average complexty of the directory is: "+averageComplexity);
+      System.out.println("The maximum complexty of the directory is: "+highestFile+" with: "+highestCompexity);
     } catch (Exception e) {
         e.printStackTrace();
     }
